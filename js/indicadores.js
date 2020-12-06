610f039988221c5790ac800cf56cbb619d9640fc
kuraistarkiller <kurai.vk@gmail.com>
2020-12-06 19:04:26 -0300

    funcionalidad terminada

--- js/indicadores.js
+++ js/indicadores.js
@@ -1,65 +1,74 @@
 function loadDolar() {
-
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://mindicador.cl/api/dolar', true);
-
+    xhr.open("GET", "https://mindicador.cl/api/dolar", true);
+  
     xhr.onload = function () {
+      let nombre = "Dolar Observado";
+      if (xhr.status >= 200 && xhr.status < 400) {
         let data = JSON.parse(xhr.responseText);
-        let nombre = 'Dolar Observado';
-        console.log(data.serie[0].valor);
         agregarElemento(nombre, data.serie[0].valor);
+      } else {
+          agregarElemento(nombre, 'Temporalmente no disponible');
+      }
     }
-
+  
+    xhr.onerror = function(){console.log("Problemas en la red")};
+  
     xhr.send();
-
 }
 
 function loadUF() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://mindicador.cl/api/uf", true);
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://mindicador.cl/api/uf', true);
-
-    xhr.onload = function () {
-        let data = JSON.parse(xhr.responseText);
-        let nombre = 'UF';
-        console.log(data.serie[0].valor);
-        agregarElemento(nombre, data.serie[0].valor);
+  xhr.onload = function () {
+    let nombre = "Valor UF";
+    if (xhr.status >= 200 && xhr.status < 400) {
+      let data = JSON.parse(xhr.responseText);
+      agregarElemento(nombre, data.serie[0].valor);
+    } else {
+        agregarElemento(nombre, 'Temporalmente no disponible');
     }
+  }
 
+  xhr.onerror = function(){console.log("Problemas en la red")};
+
+  xhr.send();
+}
+
+function loadUTM() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://mindicador.cl/api/utm", true);
+  
+    xhr.onload = function () {
+      let nombre = "Valor UTM";
+      if (xhr.status >= 200 && xhr.status < 400) {
+        let data = JSON.parse(xhr.responseText);
+        agregarElemento(nombre, data.serie[0].valor);
+      } else {
+          agregarElemento(nombre, 'Temporalmente no disponible');
+      }
+    }
+  
+    xhr.onerror = function(){console.log("Problemas en la red")};
+  
     xhr.send();
-
 }
 
 function agregarElemento(nombre, valor) {
     const listaVentas = document.querySelector("#lista-indicadores");
-
+  
     const fila = document.createElement("tr");
-
+  
     fila.innerHTML = `
-    <td>${nombre}</td>
-    <td>$ ${valor} .- CLP </td>
-    `;
+      <td>${nombre}</td>
+      <td>$ ${valor} .- CLP </td>
+      `;
     listaVentas.appendChild(fila);
-}
-
-function loadUTM() {
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://mindicador.cl/api/utm', true);
-
-    xhr.onload = function () {
-        let data = JSON.parse(xhr.responseText);
-        let nombre = 'UTM';
-        console.log(data.serie[0].valor);
-        agregarElemento(nombre, data.serie[0].valor);
-    }
-
-    xhr.send();
-
-}
+  }
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    loadDolar();
-    loadUF();
-    loadUTM();
+  loadDolar();
+  loadUF();
+  loadUTM();
 });
