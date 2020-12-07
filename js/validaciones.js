301b13bc25184025e93944461bf327b524cf029f
kuraistarkiller <kurai.vk@gmail.com>
2020-12-07 03:24:06 -0300

    formato rut y validacion oninput

--- js/validaciones.js
+++ js/validaciones.js
@@ -70,6 +70,21 @@
   
   }
 
+  function validarRut(){
+    const rutValue = rut.value.trim(); 
+  
+    if(rutValue === '' || !checkRut(rutValue)){
+      rut.classList.add('is-invalid');
+      rut.classList.remove('is-valid');
+    } else {
+      rut.classList.remove('is-invalid');
+      rut.classList.add('is-valid');
+      
+    }
+  
+  }
+  
+
 
   function validarCorreo(){
     const correoValue = correo.value.trim();
@@ -88,7 +103,102 @@
   }
   
   
+  
 
+ // Permitir sólo números en el input
+function isNumber(evt) {
+  var charCode = evt.which ? evt.which : event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode === 75) return false;
+
+  return true;
+}
+
+function checkRut(rut) {
+
+  if (rut.value.length <= 1) {
+    rut.classList.remove('is-valid');
+    rut.classList.add('is-invalid');
+    
+  }
+
+  // Obtiene el valor ingresado quitando puntos y guión.
+  var valor = clean(rut.value);
+
+  // Divide el valor ingresado en dígito verificador y resto del RUT.
+  cuerpo = valor.slice(0, -1);
+  dv = valor.slice(-1).toUpperCase();
+
+  // Separa con un Guión el cuerpo del dígito verificador.
+  rut.value = format(rut.value);
+
+  // Si no cumple con el mínimo ej. (n.nnn.nnn)
+  if (cuerpo.length < 7) {
+    rut.setCustomValidity("RUT Incompleto");
+    rut.classList.remove('is-valid');
+    rut.classList.add('is-invalid');
+    return false;
+  }
+
+  // Calcular Dígito Verificador "Método del Módulo 11"
+  suma = 0;
+  multiplo = 2;
+
+  // Para cada dígito del Cuerpo
+  for (i = 1; i <= cuerpo.length; i++) {
+    // Obtener su Producto con el Múltiplo Correspondiente
+    index = multiplo * valor.charAt(cuerpo.length - i);
+
+    // Sumar al Contador General
+    suma = suma + index;
+
+    // Consolidar Múltiplo dentro del rango [2,7]
+    if (multiplo < 7) {
+      multiplo = multiplo + 1;
+    } else {
+      multiplo = 2;
+    }
+  }
+
+  // Calcular Dígito Verificador en base al Módulo 11
+  dvEsperado = 11 - (suma % 11);
+
+  // Casos Especiales (0 y K)
+  dv = dv == "K" ? 10 : dv;
+  dv = dv == 0 ? 11 : dv;
+
+  // Validar que el Cuerpo coincide con su Dígito Verificador
+  if (dvEsperado != dv) {
+    rut.setCustomValidity("RUT Inválido");
+
+    rut.classList.remove('is-valid');
+    rut.classList.add('is-invalid');
+
+    return false;
+  } else {
+    rut.setCustomValidity("RUT Válido");
+
+    rut.classList.remove('is-invalid');
+    rut.classList.add('is-valid');
+    return true;
+  }
+}
+
+function format (rut) {
+  rut = clean(rut)
+
+  var result = rut.slice(-4, -1) + '-' + rut.substr(rut.length - 1)
+  for (var i = 4; i < rut.length; i += 3) {
+    result = rut.slice(-3 - i, -i) + '.' + result
+  }
+
+  return result
+}
+
+function clean (rut) {
+  return typeof rut === 'string'
+    ? rut.replace(/^0+|[^0-9kK]+/g, '').toUpperCase()
+    : ''
+}
 
 
 
